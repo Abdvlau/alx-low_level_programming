3c80c3be304817896777200c362a468d47525379
Abdvlau <abdulabdullahiumar69@gmail.com>
2023-09-07 20:58:44 -0700

    101-quote.c

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+#include <unistd.h>
+/**
+ * main - A c program that prints a line to the standard error
+ * Return: Always returns 1 to indicate an error
+ */
+int main(void)
+	fput("and that piece of art is useful\" - Dora Korpar, 2015-10-19\r", stdout);
+return (1);
+}
