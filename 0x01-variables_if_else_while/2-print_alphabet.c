37539f504f3b0a704ed9f13b02bbc98f540dbc57
Abdvlau <abdulabdullahiumar69@gmail.com>
2023-09-09 09:19:23 -0700

    2-print_alphabet.c

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -1,35 +1,16 @@
 #include <stdio.h>
-#include <stdlib.h>
 /**
  * main - main block
- * Return: 0 (success)
+ * Return: 0 (Success)
  */
 int main(void)
 {
-	int c;
-	int d = 0;
-
-	while (d < 10)
-	{
-		c = 0;
-		while (c < 10)
-		{
-			if (d != c && d < c)
-			{
-				putchar('0' + d);
-				putchar('0' + c);
-
-				if (c + d != 17)
-				{
-					putchar(',');
-					putchar(' ');
-				}
-			}
-
-			c++;
-		}
-		d++;
-	}
-	putchar('\n');
-	return (0);
+char letter = 'a';
+while (letter <= 'z')
+{
+	putchar(letter);
+	letter++;
+}
+putchar('\n');
+return (0);
 }
