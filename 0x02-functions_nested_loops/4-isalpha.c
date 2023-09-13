fe313d5dd55782b270676ef49f115c713434eab6
Abdvlau <abdulabdullahiumar69@gmail.com>
2023-09-13 15:57:05 -0700

    Fix betty documentation error

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -1,9 +1,11 @@
 #include "main.h"
 /**
- * int _isalpha - checks for alphabetical character
+ * _isalpha - checks for alphabetical character
  * @c: the character to be checked
- * Return: 1 if c is a character 0 otherwise
+ *
+ * Return: 1 if c is a character, 0 otherwise
  */
+
 int _isalpha(int c)
 {
 if ((c >= 65 && c <= 90) || (c >= 97 && c <= 122))
